4a636bc42e127f6b1cea96e5065f77ffd8683a2e
Mauro Verzetti <mverzett@cern.ch>
2015-07-25 03:33:17 -0500

    analyze layer

---
+++ analyze_flat_trees.py
@@ -0,0 +1,84 @@
+import ROOT
+import pprint
+import sys
+from DataFormats.FWLite import Events, Handle
+from rootpy.plotting import Hist #yes, needs rootpy, it is MUCH better
+import rootpy.io as io
+from RecoBTag.CTagging.helpers import get_vars
+import os
+ROOT.gROOT.SetBatch()
+from pdb import set_trace
+import array
+
+#yeah, everything is pretty much hadrcoded, but this is
+#supposed to be a quick check 
+inputFile = io.root_open('flat_trees/CombinedSV_ALL.root')
+flat_tree = inputFile.tree
+tested_discriminators = [
+   'CvsL', 
+   'CvsB'
+]
+
+#book plots
+plots = {}
+for name in tested_discriminators:
+   plots[name] = {}
+   #discriminator output
+   plots[name]['output']   = Hist(400, -2, 2)
+   plots[name]['output_C'] = Hist(400, -2, 2)
+   plots[name]['output_L'] = Hist(400, -2, 2)
+   plots[name]['output_B'] = Hist(400, -2, 2)
+
+
+class FunctorFromMVA(object):
+    def __init__(self, name, xml_filename, **kwargs):
+       mva_vars = get_vars(xml_filename)
+       variables = [i.name.value() for i in mva_vars]
+       self.reader    = ROOT.TMVA.Reader( "!Color:Silent=%s:Verbose=%s" % (kwargs.get('silent','T'), kwargs.get('verbose','F')))
+       self.var_map   = {}
+       self.name      = name
+       self.variables = variables
+       self.xml_filename = xml_filename
+       for var in variables:
+          self.var_map[var] = array.array('f',[0]) 
+          self.reader.AddVariable(var, self.var_map[var])
+       self.reader.BookMVA(name, xml_filename)
+          
+    def evaluate_(self): #so I can profile the time needed
+        return self.reader.EvaluateMVA(self.name)
+
+    def __call__(self, entry):
+        #kvars enforces that we use the proper vars
+        if not ( 
+                 all(hasattr(entry, name) for name in self.variables)
+                ):
+            raise Exception("Wrong variable names. Available variables: %s" % self.variables.__repr__())
+        for name in self.variables:
+            self.var_map[name][0] = float(getattr(entry, name))
+        retval = self.evaluate_() #reader.EvaluateMVA(self.name)
+        return retval
+
+
+mva = {
+   'CvsL' : FunctorFromMVA('c_vs_l', '%s/src/RecoBTag/CTagging/data/c_vs_udsg.weight.xml' % os.environ['CMSSW_BASE']),
+   'CvsB' : FunctorFromMVA('c_vs_b', '%s/src/RecoBTag/CTagging/data/c_vs_b.weight.xml' % os.environ['CMSSW_BASE'])
+}
+
+for entry in flat_tree:
+   for short, evaluator in mva.iteritems():
+      bmva = evaluator(entry)
+      plots[short]['output'].fill(bmva)
+      flav = abs(entry.flavour)
+      if flav == 4:
+         plots[short]['output_C'].fill(bmva)
+      elif flav == 5:
+         plots[short]['output_B'].fill(bmva)
+      else:
+         plots[short]['output_L'].fill(bmva)
+
+
+with io.root_open('flat_tree_output.root', 'recreate') as out:
+   for dname, dplots in plots.iteritems():
+      tdir = out.mkdir(dname)
+      for name, plot in dplots.iteritems():
+         tdir.WriteTObject(plot, name)
---
+++ analyze_pat.py
@@ -0,0 +1,60 @@
+import ROOT
+import pprint
+import sys
+from DataFormats.FWLite import Events, Handle
+from rootpy.plotting import Hist #yes, needs rootpy, it is MUCH better
+import rootpy.io as io
+ROOT.gROOT.SetBatch()
+from pdb import set_trace
+
+#yeah, everything is pretty much hadrcoded, but this is
+#supposed to be a quick check 
+events = Events('validate_ctag_pat.root')
+tested_discriminators = {
+   'pfCombinedCvsLJetTags' : 'CvsL', 
+   'pfCombinedCvsBJetTags' : 'CvsB'
+}
+
+#book plots
+plots = {}
+for name in tested_discriminators.values():
+   plots[name] = {}
+   #discriminator output
+   plots[name]['output']   = Hist(400, -2, 2)
+   plots[name]['output_C'] = Hist(400, -2, 2)
+   plots[name]['output_L'] = Hist(400, -2, 2)
+   plots[name]['output_B'] = Hist(400, -2, 2)
+handle = Handle('std::vector<pat::Jet>')
+vtx_handle = Handle('vector<reco::Vertex>')
+
+for evt in events:
+   evt.getByLabel('selectedPatJets', handle)
+   jets = handle.product()
+
+   evt.getByLabel('goodOfflinePrimaryVertices', vtx_handle)
+   if not vtx_handle.product().size():
+      continue
+
+   for jet in jets:
+      if jet.pt() < 15 or abs(jet.eta()) > 2.5:
+         continue
+      if jet.jetFlavourInfo().getPartonFlavour() == 0:
+         continue
+
+      for full, short in tested_discriminators.iteritems():
+         bmva = jet.bDiscriminator(full)
+         plots[short]['output'].fill(bmva)
+         flav = abs(jet.jetFlavourInfo().getPartonFlavour())
+         if flav == 4:
+            plots[short]['output_C'].fill(bmva)
+         elif flav == 5:
+            plots[short]['output_B'].fill(bmva)
+         else:
+            plots[short]['output_L'].fill(bmva)
+
+
+with io.root_open('validation_output.root', 'recreate') as out:
+   for dname, dplots in plots.iteritems():
+      tdir = out.mkdir(dname)
+      for name, plot in dplots.iteritems():
+         tdir.WriteTObject(plot, name)
