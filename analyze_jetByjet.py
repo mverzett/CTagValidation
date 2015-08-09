8b41b15d5f57e20f7057ca73ed640ae3631248dd
Mauro Verzetti <mverzett@cern.ch>
2015-08-09 08:28:33 -0500

    jet by jet workflow

---
+++ analyze_jetByjet.py
@@ -0,0 +1,123 @@
+import ROOT
+import pprint
+import sys
+from DataFormats.FWLite import Events, Handle
+from rootpy.plotting import Hist #yes, needs rootpy, it is MUCH better
+import rootpy.io as io
+ROOT.gROOT.SetBatch()
+from pdb import set_trace
+import os
+import pickle
+import rootpy
+from helpers import dict2tdir, Struct, ExtJet
+log = rootpy.log["/analyze_pat"]
+log.setLevel(rootpy.log.INFO)
+from copy import deepcopy 
+
+#yeah, everything is pretty much hadrcoded, but this is
+#supposed to be a quick check 
+events = Events('trees/validate_ctag_pat.root')
+
+debug_CvsL_file = io.root_open('trees/ctag_debug_CvsL.root')
+debug_CvsB_file = io.root_open('trees/ctag_debug_CvsB.root')
+debug_CvsL = debug_CvsL_file.tree.__iter__()
+debug_CvsB = debug_CvsB_file.tree.__iter__()
+
+tested_discriminators = {
+   'pfCombinedCvsLJetTags' : 'CvsL', 
+   'pfCombinedCvsBJetTags' : 'CvsB'
+}
+
+if not os.path.isdir('analyzed'):
+   os.makedirs('analyzed')
+
+#book plots
+from plots import plots
+diff_plots = {}
+diff_plots['njets'] = Hist(40, -20, 20)
+for name, h in plots['trainingvars'].iteritems():
+   delta = h[-1].x.high - h[0].x.low
+   diff_plots[name] = Hist(100, -delta/2., delta/2)
+plots = diff_plots
+
+#import jets for flat trees
+jet_maps = {}
+with open('flat_jet_map') as infile:
+   jet_maps = pickle.load(infile)
+
+print 'build jet by jet map from flat tree'
+flat_map = {}
+inputFile = io.root_open('trees/CombinedSV_ALL.root')
+flat_tree = inputFile.tree
+
+for entry in flat_tree:
+   evtid = (entry.run, entry.lumi, entry.evt)
+   if (entry.jetPt, entry.jetEta) not in jet_maps[evtid]:
+      continue
+
+   if evtid not in flat_map: flat_map[evtid] = {}
+   flat_map[evtid][(entry.jetPt, entry.jetEta)] = entry
+
+
+handle = Handle('std::vector<pat::Jet>')
+vtx_handle = Handle('vector<reco::Vertex>')
+print 'analyzing pat output'
+for evt in events:
+   evtid = (evt.eventAuxiliary().run(), evt.eventAuxiliary().luminosityBlock(), evt.eventAuxiliary().event())
+
+   evt.getByLabel('selectedPatJets', handle)
+   jets = handle.product()
+   ext_jets = []
+   for jet in jets:
+      ext_jets.append(
+         ExtJet(
+            jet,
+            debug_CvsL.next(),
+            debug_CvsB.next()
+         )
+         )
+   jets = ext_jets
+
+   if evtid not in jet_maps:
+      log.warning("Event %s not found in the flat map!" % evtid.__repr__())
+      continue
+   
+   #jet selection
+   jets = [jet for jet in jets
+           if jet.jet.pt() > 15 
+           if abs(jet.jet.eta()) < 2.5
+           #if jet.jetFlavourInfo().getPartonFlavour() != 0
+           ]
+
+   #apply flat jet mapping
+   evt_jets = jet_maps[evtid]
+   sjets = [i for i in jets
+           if (i.jet.pt(), i.jet.eta()) in evt_jets]
+   jdiff = len(evt_jets)-len(sjets)
+   if jdiff:
+      log.warning("I could not find %i jets in Event %s" % (jdiff, evtid.__repr__()))
+      set_trace()
+
+   pat_jets = sjets
+   if not pat_jets:
+      continue
+
+   #get flat jets
+   flat_jets = flat_map[evtid]
+
+   plots['njets'].fill(len(jets) - len(flat_jets))
+   for extjet in pat_jets:
+      pat_jet = extjet.jet
+      try:
+         flat_jet = flat_jets[(pat_jet.pt(), pat_jet.eta())]
+      except:
+         set_trace()
+
+      taginfos = extjet.CvsL
+      for vname in plots:
+         if vname == 'njets': continue
+         plots[vname].fill(taginfos[vname] - getattr(flat_jet, vname))
+
+print 'saving histograms'
+with io.root_open('analyzed/jet_by_jet_diff.root', 'recreate') as out:
+   dict2tdir(plots, out)
---
+++ make_diff_plots.py
@@ -0,0 +1,73 @@
+import rootpy.io as io
+import rootpy.plotting as plotting
+import rootpy.plotting.views as views
+from rootpy.plotting.style import set_style
+#set_style('cmstdr')
+import os
+import ROOT
+from pdb import set_trace
+from RecoBTag.CTagging.helpers import get_vars
+from helpers import mkdir
+
+ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptTitle(0)
+
+
+plotdir = 'diff_%s' % os.environ['tag']
+train_var_out = {
+   -1 : '%s/single_value' % plotdir,
+    0 : '%s/vectorial/0th' % plotdir,
+    1 : '%s/vectorial/1st' % plotdir,
+    2 : '%s/vectorial/2nd' % plotdir,
+    3 : '%s/vectorial/3rd+' % plotdir
+}
+for i in train_var_out.values():
+   mkdir(i)
+
+infile = io.root_open('analyzed/jet_by_jet_diff.root')
+inview = views.StyleView(
+   infile,
+   fillstyle = 'hollow',
+   linewidth = 2,
+   markerstyle= 0,
+   drawstyle='hist',
+   legendstyle='l',
+   linecolor = 'black'
+   )
+histos = [i.name for i in infile.keys()]
+
+for path in histos:
+   plot_dir = ''
+   if path[-1].isdigit():
+      idx = int(path.split('_')[-1])
+      plot_dir = train_var_out.get(idx, train_var_out[3])
+   else:
+      plot_dir = train_var_out[-1]
+
+   canvas = plotting.Canvas(800, 600)
+   #print  flavor, name
+   #set_trace()
+
+   histo = inview.Get(path)
+   histo.title = histo.name
+   histo.Draw()
+   
+   canvas.SaveAs('%s/%s.png' % (plot_dir, path))
+   canvas.SaveAs('%s/%s.pdf' % (plot_dir, path))
+
+##    to_keep = []
+##    first = True
+##    for info, color in zip(shapes.iteritems(), colors):
+##       dname, shape = info
+##       shape.title = dname
+##       shape.linecolor = color
+##       opts = '' if first else 'same'
+##       shape.Draw(opts)
+##       first = False
+##       legend.AddEntry(shape)
+##       to_keep.append(shape)
+##    legend.Draw()
+##    canvas.SaveAs("plots/%sjet_shapes.png" % name)
+##    canvas.SaveAs("plots/%sjet_shapes.pdf" % name)   
+## pat_input.close()
