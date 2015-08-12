34c49dee522611908e575b9f9bc4e1d007b27e4c
Mauro Verzetti <mverzett@cern.ch>
2015-08-12 07:18:06 -0500

    make summary plots and tables

--- make_diff_plots.py
+++ make_diff_plots.py
@@ -45,19 +45,37 @@ for path in histos:
    else:
       plot_dir = train_var_out[-1]
 
-   canvas = plotting.Canvas(800, 600)
-   canvas.set_logy(True)
+   if path == 'SameValRate' or path == 'DiffAndDefaulted':      
+      canvas = plotting.Canvas(1500, 900)
+      canvas.set_top_margin(0.001)
+      canvas.set_bottom_margin(0.5)
+   else:
+      canvas = plotting.Canvas(800, 600)
+      
    #print  flavor, name
    #set_trace()
 
    histo = inview.Get(path)
    histo.title = histo.name
    m = max(i.value for i in histo)
-   histo.yaxis.range_user = (0.1, 3*m)
+   if path == 'SameValRate' or path == 'DiffAndDefaulted':
+      histo.yaxis.range_user = (0.8, 1.3)
+   else:
+      canvas.set_logy(True)
+      histo.yaxis.range_user = (0.1, 3*m)
+
    histo.Draw()
    
    canvas.SaveAs('%s/%s.png' % (plot_dir, path))
    canvas.SaveAs('%s/%s.pdf' % (plot_dir, path))
+
+with open('%s/same_value_rate.raw_txt' % plot_dir, 'w') as table:
+   histo = inview.Get('SameValRate')
+   labels = [histo.xaxis.get_bin_label(i) for i, _ in enumerate(histo)]
+   mlen = max(len(i) for i in labels)
+   format = '%-'+str(mlen)+'s %.2f\n'
+   for bin, label in zip(histo, labels):
+      table.write(format % (label, bin.value))
 
 ##    to_keep = []
 ##    first = True
